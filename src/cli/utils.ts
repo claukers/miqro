9c8f3926aaf913489efdd34eb29f49e82a66cd9a
claukers <claukers@github>
2021-08-10 00:51:23 -0400

    remove checkModule

--- src/cli/utils.ts
+++ src/cli/utils.ts
@@ -1,13 +1,5 @@
 import cp from "child_process";
 
-export const checkModule = (module: string, dev?: boolean): any => {
-  try {
-    return require(module);
-  } catch (e) {
-    throw new Error(`missing ${module} module\nrun npm install ${module} --save${dev ? "-dev" : ""}`);
-  }
-}
-
 export const execSync = (cmd: string, options?: cp.ExecSyncOptionsWithBufferEncoding): void => {
   console.log(cmd);
   cp.execSync(
