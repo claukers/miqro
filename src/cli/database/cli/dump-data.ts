0f765b2376eb6f5029b06d64fff8f4574f0bc9b2
claukers <claukers@github>
2021-08-24 06:51:57 -0400

    limit count on dumpdata

--- src/cli/database/cli/dump-data.ts
+++ src/cli/database/cli/dump-data.ts
@@ -1,4 +1,4 @@
-import { loadConfig, SimpleMap } from "@miqro/core";
+import { checkEnvVariables, loadConfig, SimpleMap } from "@miqro/core";
 import { resolve } from "path";
 import { writeFileSync } from "fs";
 import { loadSequelize } from "@miqro/database";
@@ -7,11 +7,18 @@ export const main = async (): Promise<void> => {
   const logger = console;
   const outfile = process.argv[3];
   if (process.argv.length !== 4) {
-    throw new Error(`arguments: <outfile>`);
+    throw new Error(`[LIMIT_COUNT=100] arguments: <outfile>`);
   }
 
   if (typeof outfile !== "string") {
     throw new Error(`<outfile> must be a string!`);
+  }
+
+  const [LIMIT_COUNT] = checkEnvVariables(["LIMIT_COUNT"], ["100"]);
+  const limit = parseInt(LIMIT_COUNT, 10);
+
+  if (!isNaN(limit) || limit <= 0) {
+    throw new Error(`LIMIT_COUNT must be a number grater than 0!`);
   }
 
   loadConfig();
@@ -20,8 +27,24 @@ export const main = async (): Promise<void> => {
   logger.info(`beware that if the model is not implicitly defined in db.models it will be dumped.`);
   const models = Object.keys(db.models);
   for (const modelName of models) {
-    const rows = await db.models[modelName].findAll();
-    out[modelName] = rows;
+    let rows;
+    let offset = 0;
+    out[modelName] = [];
+    while ((rows = await db.models[modelName].findAndCountAll({
+      offset,
+      limit
+    })).rows.length > 0) {
+      out[modelName] = out[modelName].concat(rows.map(r => {
+        const ret = r;
+        for (const v of r.dataValues) {
+          if (r.v instanceof Buffer) {
+            ret[v] = r.v.toString("utf-8");
+          }
+        }
+        return ret;
+      }));
+      offset += limit;
+    }
   }
   await db.close();
   writeFileSync(resolve(process.cwd(), outfile), JSON.stringify(out, undefined, 2));
