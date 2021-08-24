d0ed315a6d6bc4a8e7cd80a01ad989d4df349359
claukers <claukers@github>
2021-08-24 01:55:10 -0400

    push-data BULK_CREATE_COUNT env var

--- src/cli/database/cli/push-data.ts
+++ src/cli/database/cli/push-data.ts
@@ -1,4 +1,4 @@
-import { loadConfig, SimpleMap } from "@miqro/core";
+import { checkEnvVariables, loadConfig, SimpleMap } from "@miqro/core";
 import { resolve } from "path";
 import { readFileSync } from "fs";
 import { loadSequelize } from "@miqro/database";
@@ -7,7 +7,7 @@ export const main = async (): Promise<void> => {
   const outfile = process.argv[3];
   const models = process.argv[4];
   if (process.argv.length !== 5) {
-    throw new Error(`arguments: <outfile> <modelA,..>`);
+    throw new Error(`[BULK_CREATE_COUNT=100] arguments: <outfile> <modelA,..>`);
   }
 
   if (typeof outfile !== "string") {
@@ -18,15 +18,31 @@ export const main = async (): Promise<void> => {
     throw new Error(`<modelA,..> must be a list of model names!`);
   }
 
-  const modelList = models.split(",").map(o => o.trim());
+  const [BULK_CREATE_COUNT] = checkEnvVariables(["BULK_CREATE_COUNT"], ["100"]);
+  const bulkCount = parseInt(BULK_CREATE_COUNT, 10);
 
+  if (!isNaN(bulkCount) || bulkCount < 0) {
+    throw new Error(`BULK_CREATE_COUNT must be a number grater or equal than 0!`);
+  }
+
+  const modelList = models.split(",").map(o => o.trim());
 
   loadConfig();
   const db = loadSequelize();
   const out: SimpleMap<any[]> = JSON.parse(readFileSync(resolve(process.cwd(), outfile)).toString());
   for (const modelName of modelList) {
     if (out[modelName] && db.models[modelName]) {
-      await db.models[modelName].bulkCreate(out[modelName]);
+      const list = out[modelName];
+      if (bulkCount === 0) {
+        for (const m of list) {
+          await db.models[modelName].create(m);
+        }
+      } else {
+        let current;
+        while ((current = list.splice(0, bulkCount)).length > 0) {
+          await db.models[modelName].bulkCreate(current);
+        }
+      }
     }
   }
   await db.close();
