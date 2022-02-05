86b43055220def38c828aa9f7518296cf045eba8
claukers <claukers@github>
2022-02-05 02:03:33 -0300

    add proxy to serve

--- src/cmds/serve.ts
+++ src/cmds/serve.ts
@@ -1,24 +1,62 @@
-import {App, loadConfig, Static, LoggerHandler} from "@miqro/core";
+import {App, loadConfig, LoggerHandler, Proxy, ReadBuffer, Static} from "@miqro/core";
+import {extractFlags} from "../utils";
+import {URL} from "url";
+import {normalizePath} from "@miqro/core/dist/common/tokenize-match";
 
 export const main = (): void => {
+  const flags = extractFlags(process.argv.slice(3), {
+    flags: {
+      "proxy": {
+        description: "proxy",
+        hasValue: true
+      },
+      "port": {
+        description: "port",
+        hasValue: true
+      }
+    }
+  });
 
-  if (process.argv.length <= 3 || process.argv.length > 5) {
-    throw new Error(`invalid number of args\nusage: [PORT=8080] npx miqro serve <directory> [path=/]`);
+  if (flags.files.length > 2) {
+    throw new Error(`invalid number of args\nusage: [PORT=8080] npx miqro serve [directory=./] [path=/]`);
   }
 
-  const directory = process.argv[3];
-  const path = process.argv[4] ? process.argv[4] : "/";
+  if (flags.flags.port instanceof Array) {
+    throw new Error(`invalid number of args\nusage: [PORT=8080] npx miqro serve [directory=./] [path=/] [--port 8080] [--proxy /api=https://host/api]`);
+  }
+
+  let [directory, path] = flags.files;
+  path = path ? path : "/";
+  directory = directory ? directory : process.cwd();
 
   loadConfig();
 
-  const PORT = process.env.PORT ? process.env.PORT : 8080;
+  const PORT = flags.flags.port ? flags.flags.port : (process.env.PORT ? process.env.PORT : 8080);
 
   if (PORT === undefined) {
-    throw new Error("PORT env var not defined");
+    throw new Error("port not defined");
   }
 
   const app = new App();
   app.use(LoggerHandler());
+
+  const proxyList = flags.flags.proxy instanceof Array ? flags.flags.proxy : [flags.flags.proxy];
+  for (const proxy of proxyList) {
+    if (proxy) {
+      const proxySplit = proxy.split("=");
+      if (proxySplit.length !== 2) {
+        throw new Error("proxy must be in the form. /path=proxy");
+      }
+      const proxyPath = normalizePath(proxySplit[0]);
+      const proxyURL = new URL(proxySplit[1]);
+      proxyURL.pathname = normalizePath(proxyURL.pathname);
+      app.use(ReadBuffer());
+      app.use(Proxy({
+        url: proxyURL.toString()
+      }), proxyPath);
+    }
+  }
+
   app.use(Static({
     directory,
     list: true
