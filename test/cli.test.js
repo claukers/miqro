96ac10887a73fe6468e7e50451f0fd5178fc950d
claukers <claukers@github>
2022-04-17 02:27:00 -0400

    update tests

--- test/cli.test.js
+++ test/cli.test.js
@@ -1,67 +1,68 @@
-const { mainCMD, extractFlags } = require("../dist/utils");
-const { strictEqual } = require("assert");
-const { fake } = require("@miqro/test");
+const {mainCMD, extractFlags} = require("../dist/utils");
+const {strictEqual} = require("assert");
+const {fake} = require("@miqro/test");
 
+describe("cli functional tests", () => {
+  it("cli happy path", async () => {
+    const cb = fake(() => {
 
-it("cli happy path", async () => {
-  const cb = fake(() => {
-
+    });
+    mainCMD({
+      "cmd": {
+        description: "description",
+        cb
+      }
+    }, "usage", console, "cmd");
+    strictEqual(cb.callCount, 1);
   });
-  mainCMD({
-    "cmd": {
-      description: "description",
-      cb
-    }
-  }, "usage", console, "cmd");
-  strictEqual(cb.callCount, 1);
-});
 
-it("cli happy path cmd not found", async () => {
-  const cb = fake(() => {
+  it("cli happy path cmd not found", async () => {
+    const cb = fake(() => {
 
+    });
+    mainCMD({
+      "cmd": {
+        description: "description",
+        cb
+      }
+    }, "usage", console, "cmd2", false);
+    strictEqual(cb.callCount, 0);
   });
-  mainCMD({
-    "cmd": {
-      description: "description",
-      cb
-    }
-  }, "usage", console, "cmd2", false);
-  strictEqual(cb.callCount, 0);
-});
 
-it("cli happy path extract flags", async () => {
-  const ret = extractFlags(["-b", "b", "cmd1", "cmd2", "cmd3", "--b-b", "bb", "cmd4", "-c-c", "ccc", "-n", "n1", "-n", "n2"]);
-  strictEqual(ret.files.length, 4);
-  strictEqual(Object.keys(ret.flags).length, 4);
-  strictEqual(ret.flags.n[0], "n1");
-  strictEqual(ret.flags.n[1], "n2");
-  strictEqual(ret.flags.b, "b");
-  strictEqual(ret.flags["c-c"], "ccc");
-  strictEqual(ret.flags["b-b"], "bb");
-});
-
-it("cli happy path cmd not command", async () => {
-  const cb = fake(() => {
-
+  it("cli happy path extract flags", async () => {
+    const ret = extractFlags(["-b", "b", "cmd1", "cmd2", "cmd3", "--b-b", "bb", "cmd4", "-c-c", "ccc", "-n", "n1", "-n", "n2"]);
+    strictEqual(ret.files.length, 4);
+    strictEqual(Object.keys(ret.flags).length, 4);
+    strictEqual(ret.flags.n[0], "n1");
+    strictEqual(ret.flags.n[1], "n2");
+    strictEqual(ret.flags.b, "b");
+    strictEqual(ret.flags["c-c"], "ccc");
+    strictEqual(ret.flags["b-b"], "bb");
   });
-  mainCMD({
-    "cmd": {
-      description: "description",
-      cb
-    }
-  }, "usage", console, "", false);
-  strictEqual(cb.callCount, 0);
-});
 
-it("cli happy path cmd throws", async () => {
-  const cb = fake(() => {
-    throw new Error("bla");
+  it("cli happy path cmd not command", async () => {
+    const cb = fake(() => {
+
+    });
+    mainCMD({
+      "cmd": {
+        description: "description",
+        cb
+      }
+    }, "usage", console, "", false);
+    strictEqual(cb.callCount, 0);
   });
-  mainCMD({
-    "cmd": {
-      description: "description",
-      cb
-    }
-  }, "usage", console, "cmd", false);
-  strictEqual(cb.callCount, 1);
+
+  it("cli happy path cmd throws", async () => {
+    const cb = fake(() => {
+      throw new Error("bla");
+    });
+    mainCMD({
+      "cmd": {
+        description: "description",
+        cb
+      }
+    }, "usage", console, "cmd", false);
+    strictEqual(cb.callCount, 1);
+  });
 });
