e36935395b06c6b563f602921d1292136f092dcd
claukers <claukers@github>
2025-05-06 14:16:36 -0400

    first tests

---
+++ test/cases/simple.editor.toolbar.button.hide.test.js
@@ -0,0 +1,103 @@
+import { describe, it, beforeEach as before, afterEach as after } from "node:test";
+import { strictEqual } from "assert";
+import { createElement } from "@miqro/jsx";
+import { Editor } from "../../build/esm/editor/components/editor.js";
+import { createNodeRuntime, HTMLEncode } from "@miqro/jsx-node";
+import { initGlobals } from "../../build/esm/src/lib.js";
+import { randomUUID } from "crypto";
+
+describe("simple editor toolbar button hide tests", () => {
+  before(async () => {
+    initGlobals();
+  });
+  after(async () => {
+  });
+
+  it("case 1 disablelog:true disablepreview:true, disablereload:true", async () => {
+    const runtime = createNodeRuntime();
+    const root = runtime.createElement("root");
+    const container = runtime.createContainer(root);
+    const uuid = randomUUID();
+    const migrations = [];
+    const services = [];
+    const errors = [];
+    const files = [];
+    container.render(createElement(Editor, {
+      disablelog: "true",
+      disablepreview: "true",
+      disablereload: "true",
+      class: "main-container",
+      reloadString: `${uuid}`,
+      migrations: `${JSON.stringify(migrations)}`,
+      services: `${JSON.stringify(services)}`,
+      errors: `${JSON.stringify(errors)}`,
+      files: `${JSON.stringify(files)}`
+    }));
+    //strictEqual(root.toString(), "");
+    //console.dir(runtime.getElementById("reload-btn"));
+    strictEqual(runtime.getElementById("closeall-btn") !== null, true);
+    strictEqual(runtime.getElementById("reload-btn") !== null, false);
+    strictEqual(runtime.getElementById("preview-btn") !== null, false);
+    strictEqual(runtime.getElementById("log-btn") !== null, false);
+    container.disconnect();
+  });
+
+  it("case 2 disablelog:true disablepreview:false, disablereload:true", async () => {
+    const runtime = createNodeRuntime();
+    const root = runtime.createElement("root");
+    const container = runtime.createContainer(root);
+    const uuid = randomUUID();
+    const migrations = [];
+    const services = [];
+    const errors = [];
+    const files = [];
+    container.render(createElement(Editor, {
+      disablelog: "true",
+      //disablepreview: "true",
+      disablereload: "true",
+      class: "main-container",
+      reloadString: `${uuid}`,
+      migrations: `${JSON.stringify(migrations)}`,
+      services: `${JSON.stringify(services)}`,
+      errors: `${JSON.stringify(errors)}`,
+      files: `${JSON.stringify(files)}`
+    }));
+    //strictEqual(root.toString(), "");
+    //console.dir(runtime.getElementById("reload-btn"));
+    strictEqual(runtime.getElementById("closeall-btn") !== null, true);
+    strictEqual(runtime.getElementById("reload-btn") !== null, false);
+    strictEqual(runtime.getElementById("preview-btn") !== null, true);
+    strictEqual(runtime.getElementById("log-btn") !== null, false);
+    container.disconnect();
+  });
+
+  it("case 3 disablelog:true disablepreview:false, disablereload:false", async () => {
+    const runtime = createNodeRuntime();
+    const root = runtime.createElement("root");
+    const container = runtime.createContainer(root);
+    const uuid = randomUUID();
+    const migrations = [];
+    const services = [];
+    const errors = [];
+    const files = [];
+    container.render(createElement(Editor, {
+      disablelog: "true",
+      //disablepreview: "true",
+      //disablereload: "true",
+      class: "main-container",
+      reloadString: `${uuid}`,
+      migrations: `${JSON.stringify(migrations)}`,
+      services: `${JSON.stringify(services)}`,
+      errors: `${JSON.stringify(errors)}`,
+      files: `${JSON.stringify(files)}`
+    }));
+    //strictEqual(root.toString(), "");
+    //console.dir(runtime.getElementById("reload-btn"));
+    strictEqual(runtime.getElementById("closeall-btn") !== null, true);
+    strictEqual(runtime.getElementById("reload-btn") !== null, true);
+    strictEqual(runtime.getElementById("preview-btn") !== null, true);
+    strictEqual(runtime.getElementById("log-btn") !== null, false);
+    container.disconnect();
+  });
+
+});
--- test/cases/simple.html.inflate.test.js
+++ test/cases/simple.html.inflate.test.js
@@ -1,21 +1,105 @@
-import { describe, it } from "node:test";
+import { describe, it, beforeEach as before, afterEach as after } from "node:test";
 import { strictEqual } from "assert";
 import { Miqro } from "../../build/esm/src/lib.js";
 
 describe("simple html inflate", () => {
-  it("case 1 editor: false", async () => {
-    const port = 9999;
-    const app = new Miqro({
+  const port = 9999;
+  let app;
+  before(async () => {
+    //console.log("before");
+    if (app) {
+      await app.stop();
+    }
+    app = null;
+  });
+  after(async () => {
+    //console.log("after");
+    try {
+      if (app) {
+        await app.stop();
+        app.dispose();
+      }
+      app = null;
+    } catch (e) {
+      console.error(e);
+    }
+  });
+
+  it("case 1 editor: false hotreload:false", async () => {
+    try {
+      app = new Miqro({
+        editor: false,
+        hotreload: false,
+        port,
+        name: "case 1",
+        services: ["test/test-data/jsx"]
+      });
+
+      await app.inflate();
+      await app.start();
+
+      const response = await fetch(`http://localhost:${port}/jsx-case0.html`);
+
+      strictEqual(response.ok, true);
+      const data = (await response.text()).split(" ").map(h => h.trim().split("\n").map(h => h.trim()).join("")).join("");
+      console.dir(data);
+      strictEqual(data, "<!DOCTYPEhtml><html><body><p>Hello&#32;World&#33;</p></body></html>");
+      strictEqual(response.ok, true);
+      console.dir("asdd");
+    } catch (e) {
+      console.error(e);
+    }
+
+  });
+
+  it("case 2 editor: false hotreload:true", async () => {
+    app = new Miqro({
       editor: false,
-      hotreload: false,
+      hotreload: true,
       port,
-      name: "case 1 editor:false",
+      name: "case 2",
       services: ["test/test-data/jsx"]
     });
     await app.inflate();
     await app.start();
     const response = await fetch(`http://localhost:${port}/jsx-case0.html`);
     strictEqual(response.ok, true);
-    await app.stop();
+    const data = (await response.text()).split(" ").map(h => h.trim().split("\n").map(h => h.trim()).join("")).join("");
+    strictEqual(data, '<!DOCTYPEhtml><html><body><p>Hello&#32;World&#33;</p></body></html><script>//CreateWebSocketconnection.functiongetSocket(){returnnewWebSocket("/hot-reload");}constsocket=getSocket();lettimeout;functiontryConnection(){try{constnewSocket=getSocket();newSocket.addEventListener("open",(event)=>{console.log("reloading");window.location.reload();});newSocket.addEventListener("error",(err)=>{console.error(err);timeout=setTimeout(tryConnection,500);});}catch(e){console.error(e);timeout=setTimeout(tryConnection,500);}}//Connectionclosedsocket.addEventListener("close",(event)=>{clearTimeout(timeout);timeout=setTimeout(tryConnection,500);});socket.addEventListener("error",(err)=>{console.error(err);});</script>');
+    strictEqual(response.ok, true);
+  });
+
+  it("case 3 editor: true hotreload:false", async () => {
+    app = new Miqro({
+      editor: true,
+      hotreload: false,
+      port,
+      name: "case 3",
+      services: ["test/test-data/jsx"]
+    });
+    await app.inflate();
+    await app.start();
+    const response = await fetch(`http://localhost:${port}/jsx-case0.html`);
+    strictEqual(response.ok, true);
+    const data = (await response.text()).split(" ").map(h => h.trim().split("\n").map(h => h.trim()).join("")).join("");
+    strictEqual(data, "<!DOCTYPEhtml><html><body><p>Hello&#32;World&#33;</p></body></html>");
+    strictEqual(response.ok, true);
+  });
+
+  it("case 5 editor: true hotreload:true", async () => {
+    app = new Miqro({
+      editor: true,
+      hotreload: true,
+      port,
+      name: "case 3",
+      services: ["test/test-data/jsx"]
+    });
+    await app.inflate();
+    await app.start();
+    const response = await fetch(`http://localhost:${port}/jsx-case0.html`);
+    strictEqual(response.ok, true);
+    const data = (await response.text()).split(" ").map(h => h.trim().split("\n").map(h => h.trim()).join("")).join("");
+    strictEqual(data, '<!DOCTYPEhtml><html><body><p>Hello&#32;World&#33;</p></body></html><script>//CreateWebSocketconnection.functiongetSocket(){returnnewWebSocket("/hot-reload");}constsocket=getSocket();lettimeout;functiontryConnection(){try{constnewSocket=getSocket();newSocket.addEventListener("open",(event)=>{console.log("reloading");window.location.reload();});newSocket.addEventListener("error",(err)=>{console.error(err);timeout=setTimeout(tryConnection,500);});}catch(e){console.error(e);timeout=setTimeout(tryConnection,500);}}//Connectionclosedsocket.addEventListener("close",(event)=>{clearTimeout(timeout);timeout=setTimeout(tryConnection,500);});socket.addEventListener("error",(err)=>{console.error(err);});</script>');
+    strictEqual(response.ok, true);
   });
 })
